b35fab24e1e699ec175c40cb5f2e7b337080f401
haydenleedev <haydenlee22@gmail.com>
2021-08-26 16:25:53 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import icons from '../styles/buttons.module.scss'
+import styles from '../styles/packages.module.scss'
+import * as svgComponent from '../src/components/icons'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+        <h1 className={styles.title}>Hello UJET!</h1>
+        <div>
+          <button className={`${icons['btn']} ${icons['btn-white']} ${icons['btn-big']}` }><span>Back</span></button>
+          <button className={`${icons['btn']} ${icons['btn-orange']} ${icons['btn-big']}` }><span>Continue</span></button>
+          <button className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-big']}` }><span>Buy Now</span></button>
+          <button className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-small']}` }><span>Try Now</span></button>
+          <button className={`${icons['btn']} ${icons['btn-skyblue']} ${icons['btn-medium']}` }><span>+ Compare all plans and features</span></button>
+        </div>
+          <div><svgComponent.UjetCxLogo/></div>
+          <div><svgComponent.IconX/></div>
+          <div><svgComponent.IconCheck/></div>
+          <div><svgComponent.IconCheckCircle/></div>
+          <div><svgComponent.IconInfo/></div>
+          <span className={icons.circle}></span>
+
+          <div className={styles.relative}><span className={icons['radio-btn']}></span></div>
+
+          <div className={styles['table-headline-gray']}>Includes</div>   
+          <div className={styles['table-headline-blue']}>Today's Charge</div>
+          <div className={styles.grid}>
+          <span className={`${styles.number} ${styles['align-center']}`}>1</span> 
+        </div>
+    </div>
+  )
+}
---
+++ src/components/icons/IconCheckCircle.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function SvgIconCheckCircle(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} {...props}>
+      <path
+        d="M9 0a9 9 0 109 9 9 9 0 00-9-9zM7.7 12.4L4.3 9.2l1.4-1.4 2 1.8 4.6-4.3 1.4 1.4z"
+        fill="#3398dc"
+      />
+    </svg>
+  );
+}
+
+export default SvgIconCheckCircle;
